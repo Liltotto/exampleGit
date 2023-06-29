3d139da85f539899e3179e4304e2d9a31b0428fd
Saadu <example@gmail.com>
2023-06-29 21:27:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+'use strict'
+
+// let number = 5
+// const leftBorderWidth = 1
+
+// number = 10
+// console.log(number)
+
+// const obj = {
+// 	a: 50
+// }
+
+// obj.a = 10
+// console.log(obj)
+
+// var name = 'ivan'
+
+// {
+// 	var result = 50
+// }
+
+// console.log(result)
+
+// alert(5);
+// [].push('a')
+
+// const ebj = {
+// 	abc: {
+// 		abj: {
+// 			a: 1
+// 		}
+// 	}
+// }
+
+// console.log(ebj.abc.abj['a']);
+
+
+//13 ЗАНЯТИЕ
+
+//alert('Hello')
+
+// const result = confirm('Are u here?')
+// console.log(result)
+
+// const answer = prompt('Вам есть 18?', '')
+// console.log(answer+5)
+
+// const answers = []
+
+// answers[0] = prompt('Как ваше имя?', '')
+// answers[1] = prompt('Как ваше фамилия?', '')
+// answers[2] = prompt('Сколько вам лет?', '')
+
+// document.write(answers)
+
+
+//14 Занятие
+
+// const category = 'toys'
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = 'Ivan'
+
+// alert(`Привет, ${user}`)
+
+//15 Занятие
+console.log(4 + +'5')
