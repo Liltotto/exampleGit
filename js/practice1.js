f8c190ebc42ed1cf5a32120fa89ea98f7a91c20e
Saadu <example@gmail.com>
2023-07-01 04:54:36 +0300

    end of pr1

--- js/practice1.js
+++ js/practice1.js
@@ -1,1 +1,27 @@
 const numberOfFilms = +prompt('Сколько фильмов уже посмотрели?', '')
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {
+
+	},
+	actors: {
+
+	},
+	genres: [],
+	privat: false
+}
+
+const firstQuestion = 'Один из последних просмотренных фильмов?'
+const secondQuestion = 'Насколько оцените его?'
+
+const firstAnswer1 = prompt(firstQuestion, '')
+const secondAnswer1 = prompt(secondQuestion, '')
+
+const firstAnswer2 = prompt(firstQuestion, '')
+const secondAnswer2 = prompt(secondQuestion, '')
+
+personalMovieDB.movies[firstAnswer1] = secondAnswer1
+personalMovieDB.movies[firstAnswer2] = secondAnswer2
+
+console.log(personalMovieDB)
